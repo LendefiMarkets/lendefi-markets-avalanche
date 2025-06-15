bde8f7cab4d44ce5e903dd5c8b436a6a4fd7792a
LendefiMarkets <alexei@lendefimarkets.com>
2025-06-15 00:02:25 -0700

    LendefiAssets deployment sequence and optimization

--- contracts/markets/lib/LendefiConstants.sol
+++ contracts/markets/lib/LendefiConstants.sol
@@ -42,4 +42,13 @@ library LendefiConstants {
 
     /// @notice Max assets supported by platform
     uint32 internal constant MAX_ASSETS = 3000;
+
+    /// @notice Ethereum mainnet chain ID
+    uint256 internal constant ETHEREUM_CHAIN_ID = 1;
+
+    /// @notice Ethereum mainnet USDC token address
+    address internal constant ETHEREUM_USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    /// @notice Ethereum mainnet WETH token address
+    address internal constant ETHEREUM_WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 }
